a1894b7a828cf5b4fe1cc738b3b5e9610913f987
Arthur Bonfim <bonfimarthur885@gmail.com>
2026-04-29 13:50:46 -0300

    Update Principal.java

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -47,6 +47,7 @@ public class Principal {
             MENUCD = Integer.parseInt(MENUCADASTRO);
             }
         }
+
         if (MENUPP == 2) {
             
             int MENUMV = -1;
@@ -67,107 +68,198 @@ public class Principal {
             );
             MENUMV = Integer.parseInt(MENUMOVIMENTAÇÃO);
             
-            
-            if (MENUMV == 1) {
                 
-                String CONFIRMAENTRADA = "S";  
-                
-                while (CONFIRMAENTRADA.equalsIgnoreCase("S")) {
+                if (MENUMV == 1) {
                     
-                     
-                    String NOMEPRODUTO = JOptionPane.showInputDialog(
-                        "<html><div style='text-align: center;'>"
-                        + "SEX ON THE BAR LTDA<br>"
-                        + "SISTEMA DE CONTROLE DE ESTOQUE<br><br>"
-                        + "MOVIMENTAÇÃO - ENTRADA DE PRODUTO"
-                        + "</div><br><br>"
-                        + "<div style='text-align: justify;'>"
-                        + "PRODUTO:"
-                        + "</div></html>"
-                    );
+                    String CONFIRMAENTRADA = "S";
                     
-                     
-                    String QTDEATUALSTR = JOptionPane.showInputDialog(
-                        "<html><div style='text-align: center;'>"
-                        + "SEX ON THE BAR LTDA<br>"
-                        + "SISTEMA DE CONTROLE DE ESTOQUE<br><br>"
-                        + "MOVIMENTAÇÃO - ENTRADA DE PRODUTO"
-                        + "</div><br><br>"
-                        + "<div style='text-align: justify;'>"
-                        + "PRODUTO : " + NOMEPRODUTO + "<br><br>"
-                        + "QTDE ATUAL:"
-                        + "</div></html>"
-                    );
-                    int QTDEATUAL = Integer.parseInt(QTDEATUALSTR);
-                    
-                     
-                    String QTDEENTRADASTR = JOptionPane.showInputDialog(
-                        "<html><div style='text-align: center;'>"
-                        + "SEX ON THE BAR LTDA<br>"
-                        + "SISTEMA DE CONTROLE DE ESTOQUE<br><br>"
-                        + "MOVIMENTAÇÃO - ENTRADA DE PRODUTO"
-                        + "</div><br><br>"
-                        + "<div style='text-align: justify;'>"
-                        + "PRODUTO   : " + NOMEPRODUTO + "<br>"
-                        + "QTDE ATUAL: " + QTDEATUAL + "<br><br>"
-                        + "QTDE ENTRADA:"
-                        + "</div></html>"
-                    );
-                    int QTDEENTRADA = Integer.parseInt(QTDEENTRADASTR);
-                    
-                     
-                    int QTDEFINAL = QTDEATUAL + QTDEENTRADA;
-                    
-                    
-                    String CONFIRMA = JOptionPane.showInputDialog(
-                        "<html><div style='text-align: center;'>"
-                        + "SEX ON THE BAR LTDA<br>"
-                        + "SISTEMA DE CONTROLE DE ESTOQUE<br><br>"
-                        + "MOVIMENTAÇÃO - ENTRADA DE PRODUTO"
-                        + "</div><br><br>"
-                        + "<div style='text-align: justify;'>"
-                        + "PRODUTO     : " + NOMEPRODUTO + "<br>"
-                        + "QTDE ATUAL  : " + QTDEATUAL + "<br>"
-                        + "QTDE ENTRADA: " + QTDEENTRADA + "<br>"
-                        + "QTDE FINAL  : " + QTDEFINAL + "<br><br>"
-                        + "CONFIRMA ENTRADA (S/N)?:"
-                        + "</div></html>"
-                    );
-                    
-                    
-                    if (CONFIRMA.equalsIgnoreCase("S")) {
-                        CONFIRMAENTRADA = JOptionPane.showInputDialog(
+                    while (CONFIRMAENTRADA.equalsIgnoreCase("S")) {
+                        
+                        
+                        String NOMEPRODUTO = JOptionPane.showInputDialog(
                             "<html><div style='text-align: center;'>"
                             + "SEX ON THE BAR LTDA<br>"
                             + "SISTEMA DE CONTROLE DE ESTOQUE<br><br>"
                             + "MOVIMENTAÇÃO - ENTRADA DE PRODUTO"
                             + "</div><br><br>"
                             + "<div style='text-align: justify;'>"
-                            + "ENTRADA CONFIRMADA COM SUCESSO!<br><br>"
-                            + "NOVA ENTRADA (S/N)?:"
+                            + "PRODUTO:"
                             + "</div></html>"
                         );
-                    } else {
-                         
-                        CONFIRMAENTRADA = JOptionPane.showInputDialog(
+                        
+                        
+                        String QTDEATUALSTR = JOptionPane.showInputDialog(
                             "<html><div style='text-align: center;'>"
                             + "SEX ON THE BAR LTDA<br>"
                             + "SISTEMA DE CONTROLE DE ESTOQUE<br><br>"
                             + "MOVIMENTAÇÃO - ENTRADA DE PRODUTO"
                             + "</div><br><br>"
                             + "<div style='text-align: justify;'>"
-                            + "ENTRADA CANCELADA.<br><br>"
-                            + "NOVA ENTRADA (S/N)?:"
+                            + "PRODUTO    : " + NOMEPRODUTO + "<br><br>"
+                            + "QTDE ATUAL:"
                             + "</div></html>"
                         );
+                        int QTDEATUAL = Integer.parseInt(QTDEATUALSTR);
+                        
+                        
+                        String QTDEENTRADASTR = JOptionPane.showInputDialog(
+                            "<html><div style='text-align: center;'>"
+                            + "SEX ON THE BAR LTDA<br>"
+                            + "SISTEMA DE CONTROLE DE ESTOQUE<br><br>"
+                            + "MOVIMENTAÇÃO - ENTRADA DE PRODUTO"
+                            + "</div><br><br>"
+                            + "<div style='text-align: justify;'>"
+                            + "PRODUTO    : " + NOMEPRODUTO + "<br>"
+                            + "QTDE ATUAL : " + QTDEATUAL + "<br><br>"
+                            + "QTDE ENTRADA:"
+                            + "</div></html>"
+                        );
+                        int QTDEENTRADA = Integer.parseInt(QTDEENTRADASTR);
+                        
+                        
+                        int QTDEFINAL = QTDEATUAL + QTDEENTRADA;
+                        
+                        
+                        String CONFIRMA = JOptionPane.showInputDialog(
+                            "<html><div style='text-align: center;'>"
+                            + "SEX ON THE BAR LTDA<br>"
+                            + "SISTEMA DE CONTROLE DE ESTOQUE<br><br>"
+                            + "MOVIMENTAÇÃO - ENTRADA DE PRODUTO"
+                            + "</div><br><br>"
+                            + "<div style='text-align: justify;'>"
+                            + "PRODUTO      : " + NOMEPRODUTO + "<br>"
+                            + "QTDE ATUAL   : " + QTDEATUAL + "<br>"
+                            + "QTDE ENTRADA : " + QTDEENTRADA + "<br>"
+                            + "QTDE FINAL   : " + QTDEFINAL + "<br><br>"
+                            + "CONFIRMA ENTRADA (S/N)?:"
+                            + "</div></html>"
+                        );
+                        
+                       
+                        if (CONFIRMA.equalsIgnoreCase("S")) {
+                            CONFIRMAENTRADA = JOptionPane.showInputDialog(
+                                "<html><div style='text-align: center;'>"
+                                + "SEX ON THE BAR LTDA<br>"
+                                + "SISTEMA DE CONTROLE DE ESTOQUE<br><br>"
+                                + "MOVIMENTAÇÃO - ENTRADA DE PRODUTO"
+                                + "</div><br><br>"
+                                + "<div style='text-align: justify;'>"
+                                + "ENTRADA CONFIRMADA COM SUCESSO!<br><br>"
+                                + "NOVA ENTRADA (S/N)?:"
+                                + "</div></html>"
+                            );
+                        } else {
+                            CONFIRMAENTRADA = JOptionPane.showInputDialog(
+                                "<html><div style='text-align: center;'>"
+                                + "SEX ON THE BAR LTDA<br>"
+                                + "SISTEMA DE CONTROLE DE ESTOQUE<br><br>"
+                                + "MOVIMENTAÇÃO - ENTRADA DE PRODUTO"
+                                + "</div><br><br>"
+                                + "<div style='text-align: justify;'>"
+                                + "ENTRADA CANCELADA.<br><br>"
+                                + "NOVA ENTRADA (S/N)?:"
+                                + "</div></html>"
+                            );
+                        }
                     }
-                    
-                     
                 }
+                
+                if (MENUMV == 2) {
+                    
+                    String CONFIRMASAIDA = "S";
+                    
+                    while (CONFIRMASAIDA.equalsIgnoreCase("S")) {
+                        
+                        
+                        String NOMEPRODUTOSAIDA = JOptionPane.showInputDialog(
+                            "<html><div style='text-align: center;'>"
+                            + "SEX ON THE BAR LTDA<br>"
+                            + "SISTEMA DE CONTROLE DE ESTOQUE<br><br>"
+                            + "MOVIMENTAÇÃO - SAÍDA DE PRODUTO"
+                            + "</div><br><br>"
+                            + "<div style='text-align: justify;'>"
+                            + "PRODUTO:"
+                            + "</div></html>"
+                        );
+                        
+                        
+                        String QTDEATUALSAIDASTR = JOptionPane.showInputDialog(
+                            "<html><div style='text-align: center;'>"
+                            + "SEX ON THE BAR LTDA<br>"
+                            + "SISTEMA DE CONTROLE DE ESTOQUE<br><br>"
+                            + "MOVIMENTAÇÃO - SAÍDA DE PRODUTO"
+                            + "</div><br><br>"
+                            + "<div style='text-align: justify;'>"
+                            + "PRODUTO    : " + NOMEPRODUTOSAIDA + "<br><br>"
+                            + "QTDE ATUAL:"
+                            + "</div></html>"
+                        );
+                        int QTDEATUALSAIDA = Integer.parseInt(QTDEATUALSAIDASTR);
+                        
+                        
+                        String QTDESAIDASTR = JOptionPane.showInputDialog(
+                            "<html><div style='text-align: center;'>"
+                            + "SEX ON THE BAR LTDA<br>"
+                            + "SISTEMA DE CONTROLE DE ESTOQUE<br><br>"
+                            + "MOVIMENTAÇÃO - SAÍDA DE PRODUTO"
+                            + "</div><br><br>"
+                            + "<div style='text-align: justify;'>"
+                            + "PRODUTO    : " + NOMEPRODUTOSAIDA + "<br>"
+                            + "QTDE ATUAL : " + QTDEATUALSAIDA + "<br><br>"
+                            + "QTDE SAÍDA:"
+                            + "</div></html>"
+                        );
+                        int QTDESAIDA = Integer.parseInt(QTDESAIDASTR);
+                        
+                        
+                        int QTDEFINALSAIDA = QTDEATUALSAIDA - QTDESAIDA;
+                        
+                        
+                        String CONFIRMA = JOptionPane.showInputDialog(
+                            "<html><div style='text-align: center;'>"
+                            + "SEX ON THE BAR LTDA<br>"
+                            + "SISTEMA DE CONTROLE DE ESTOQUE<br><br>"
+                            + "MOVIMENTAÇÃO - SAÍDA DE PRODUTO"
+                            + "</div><br><br>"
+                            + "<div style='text-align: justify;'>"
+                            + "PRODUTO    : " + NOMEPRODUTOSAIDA + "<br>"
+                            + "QTDE ATUAL : " + QTDEATUALSAIDA + "<br>"
+                            + "QTDE SAÍDA : " + QTDESAIDA + "<br>"
+                            + "QTDE FINAL : " + QTDEFINALSAIDA + "<br><br>"
+                            + "CONFIRMA SAÍDA (S/N)?:"
+                            + "</div></html>"
+                        );
+                        
+                        
+                        if (CONFIRMA.equalsIgnoreCase("S")) {
+                            CONFIRMASAIDA = JOptionPane.showInputDialog(
+                                "<html><div style='text-align: center;'>"
+                                + "SEX ON THE BAR LTDA<br>"
+                                + "SISTEMA DE CONTROLE DE ESTOQUE<br><br>"
+                                + "MOVIMENTAÇÃO - SAÍDA DE PRODUTO"
+                                + "</div><br><br>"
+                                + "<div style='text-align: justify;'>"
+                                + "SAÍDA CONFIRMADA COM SUCESSO!<br><br>"
+                                + "NOVA SAÍDA (S/N)?:"
+                                + "</div></html>"
+                            );
+                        } else {
+                            CONFIRMASAIDA = JOptionPane.showInputDialog(
+                                "<html><div style='text-align: center;'>"
+                                + "SEX ON THE BAR LTDA<br>"
+                                + "SISTEMA DE CONTROLE DE ESTOQUE<br><br>"
+                                + "MOVIMENTAÇÃO - SAÍDA DE PRODUTO"
+                                + "</div><br><br>"
+                                + "<div style='text-align: justify;'>"
+                                + "SAÍDA CANCELADA.<br><br>"
+                                + "NOVA SAÍDA (S/N)?:"
+                                + "</div></html>"
+                            );
+                        }
+                    }
+                }
+                
             }
-             
-            }
-        
         }
         }
     }
